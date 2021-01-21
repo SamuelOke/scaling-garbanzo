203bd3cfdcadc896ca0e33212484e2d165a035a6
SamuelOke <58989112+SamuelOke@users.noreply.github.com>
2021-01-21 11:22:45 -0700

    Add files via upload

---
+++ pythag.py
@@ -0,0 +1,10 @@
+import math
+from math import sqrt
+# Input
+
+print('Input lengths of shorter triangle sides:')
+a = float(input('Length of side a:'))
+b = float(input('Length of side b:'))
+c = sqrt(a**2 + b**2)
+
+print('The length of the Hypoteneuse is', c)
